a2908bec1bb8320eb6fbe8d6bcd1aca45e53300f
min429 <lsm990429@gmail.com>
2024-02-18 14:45:54 +0900

    Initial commit

---
+++ src/main/java/toyproject/personal/englishconversation/EnglishConversationApplication.java
@@ -0,0 +1,13 @@
+package toyproject.personal.englishconversation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishConversationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishConversationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toyproject/personal/englishconversation/EnglishConversationApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject.personal.englishconversation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishConversationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

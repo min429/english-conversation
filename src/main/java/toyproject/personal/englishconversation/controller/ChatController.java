e7c1dd85d8d0658ff4a28d0ba80a86b1f9694e54
min429 <lsm990429@gmail.com>
2024-02-26 13:27:01 +0900

    FEAT : ChatController 추가 #1

---
+++ src/main/java/toyproject/personal/englishconversation/controller/ChatController.java
@@ -0,0 +1,31 @@
+package toyproject.personal.englishconversation.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import toyproject.personal.englishconversation.controller.dto.ChatGPTRequestDto;
+import toyproject.personal.englishconversation.domain.message.GPTMessage;
+import toyproject.personal.englishconversation.service.ChatService;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+@RequestMapping("/chat")
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final ChatService chatService;
+
+    @PostMapping
+    public ResponseEntity<GPTMessage> chat(@RequestBody ChatGPTRequestDto request) throws IOException {
+        log.debug("request: {}", request);
+        GPTMessage gptMessage = chatService.processChatRequest(request);
+        return ResponseEntity.ok(gptMessage);
+    }
+
+}

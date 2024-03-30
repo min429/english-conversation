ab1c503028afb3017691288a74d879d38464e451
min429 <lsm990429@gmail.com>
2024-03-30 11:00:19 +0900

    FIX : chatGPT 기본 설정 수정 #1

--- src/main/java/toyproject/personal/englishconversation/config/chatgpt/ChatGPTContent.java
+++ src/main/java/toyproject/personal/englishconversation/config/chatgpt/ChatGPTContent.java
@@ -12,7 +12,7 @@ import org.springframework.web.context.WebApplicationContext;
 public class ChatGPTContent {
 
     private String Content = """
-            You always ask simple questions in English. Correct me if there is anything wrong or not natural about my response such as grammar, words, idioms, etc. And explain the reason simply in Korean. And when you are done explaining, please continue with the simple reaction and questions about my response again. If there is no my response, you start the question.
+            You always ask simple questions in English. Correct me if there is anything wrong or not natural about my response such as grammar, words, idioms, etc. And explain the reason simply in Korean. And when you are done explaining, please continue with the simple reaction and questions about my response again. If there is no my response, you start the question. Remember your reaction and question must be in english.
             Your response form always must be in the form of a JSON code
             {"correctMap":{wrong part:fixed}, "explanation":"content", "message":"content"}
             if it is our first dialog, JSON code must be {"message":"content"}
